446f69fb4f327323cb124947cd968aca050ac195
Romain Brault <mail@romainbrault.com>
2024-12-03 03:01:28 +0000

    feat: ✨ add command line interface

--- src/test_gh_linux_py312_test_open_source/__init__.py
+++ src/test_gh_linux_py312_test_open_source/__init__.py
@@ -4,10 +4,25 @@
 
 """Top-level module."""
 
+import importlib
 from typing import Final
 
+from test_gh_linux_py312_test_open_source.environment import (
+    ENVIRONMENT_FILE,
+    load_dotenv,
+)
 from test_gh_linux_py312_test_open_source.package_metadata import __version__
 
 
 __all__: Final = ["__version__"]
 """Public module attributes."""
+
+
+if __debug__:
+    beartype = importlib.import_module("beartype")
+    beartype_claw = importlib.import_module("beartype.claw")
+    beartype_claw.beartype_this_package(
+        conf=beartype.BeartypeConf(is_color=False),
+    )
+
+load_dotenv(ENVIRONMENT_FILE)
